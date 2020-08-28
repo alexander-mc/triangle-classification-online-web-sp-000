4f36b1737de01c1ce3050451893c35ddc4d2b680
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-28 17:12:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,9 +7,9 @@ class Triangle
   end
   
   def kind
-    if length1 <= 0 || length2 <= 0 || length3 <= 0
+    if @length1 <= 0 || @length2 <= 0 || @length3 <= 0
       raise TriangleError
-    elsif length1 + length2 < length3 || length1 + length3 < length2 || length2 + length3 < length1
+    elsif @length1 + @length2 < @length3 || @length1 + @length3 < @length2 || @length2 + @length3 < @length1
       raise TriangleError
     else
       if @length1 == @length2 && @length1 == @length3
